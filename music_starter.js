dd4b653ac350e42aa220809f2415bb9b0d4ee504
mrsloppytom <140354003+mrsloppytom@users.noreply.github.com>
2023-10-05 12:29:30 +1300

    more playin

--- music_starter.js
+++ music_starter.js
@@ -95,7 +95,25 @@ noStroke()
 
 
 
+var drumSize = map(drum, 0, 100, 0, height)
+angleMode(DEGREES)
+rectMode(CENTER)
 
+background(104, 142, 204)
+noFill()
+stroke(255)
+
+translate(width/2, height /2);
+
+for (var i = 0; i < 200; i++){
+push()
+
+rotate(sin(frameCount + i *2) * 100)
+
+rect(0, 0, 600 - i * 3, drumSize, drumSize*2)
+
+pop()
+}
 
 
 
